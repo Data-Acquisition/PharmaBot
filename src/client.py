497818e713507885f1a1df38e5a6790da7315648
Михаил Четверик <mikechetv@MacBook-Air-Mihail.local>
2023-01-25 12:04:37 +0300

    updt to class

--- src/client.py
+++ src/client.py
@@ -4,78 +4,84 @@ from collections import namedtuple
 
 url = 'https://api-test.maxma.com/'
 secret_key = "f8634ce0-5ca2-4b99-a371-110383cb3ecf"
-
 headers = {
   'Accept': 'application/json',
   'Content-Type': 'application/json',
   'Cache-Control': 'no-cache',
   'X-Processing-Key': secret_key
 }
+phone = '+79774583897'
 
-def to_dto(responce):
-  x = json.loads(responce.text, object_hook 
-                 = lambda d : namedtuple('X', d.keys())
-                 (*d.values()))
-  return x
+class Maxma():
+  def __init__(self, url, secret_key, headers, phone):
+    self.url = url
+    self.secret_key = secret_key
+    self.headers = headers
+    self.phone = phone
 
-def get_balance(url, phone):
-  url += 'get-balance'
-  data = {
-    "phoneNumber": phone
-  }
-  data_json = json.dumps(data)
-  r = requests.post(url=url,headers=headers, data=data_json)
-  respDTO = to_dto(r)
-  return respDTO.bonuses[0]
+  def to_dto(self, responce):
+    x = json.loads(responce.text, object_hook 
+                  = lambda d : namedtuple('X', d.keys())
+                  (*d.values()))
+    return x
 
-def add_new_client(url, phone, email, fullName, gender, birthdate, card):
-  url += 'new-client'
-  data = {
-    "client": {
-      "phoneNumber": phone,
-      "email": email,
-      "fullName": fullName,
-      "gender": gender,
-      "birthdate": birthdate,
-      "card": card
+  def get_balance(self):
+    url = self.url + 'get-balance'
+    data = {
+      "phoneNumber": self.phone
     }
-  }
-  data_json = json.dumps(data)
-  r = requests.post(url=url,headers=headers, data=data_json)
-  return to_dto(r)
+    r = requests.post(url=url,headers=self.headers, json=data)
+    respDTO = self.to_dto(r)
+    try:
+      return respDTO.bonuses[0].amount
+    except IndexError:
+      return 0
 
-def send_code(url, phone):
-  url += 'send-confirmation-code'
-  data = {
-    "phoneNumber": phone
-  }
-  data_json = json.dumps(data)
-  r = requests.post(url=url,headers=headers, data=data_json)
-  respDTO = to_dto(r)
-  return respDTO
-
-def change_balance(url, phone, delta):
-  url += 'adjust-balance'
-  data = {
-    "client": {
-      "phoneNumber": phone
-    },
-    "balanceAdjustment": {
-      "amountDelta": delta,
-      "expirationPeriodDays": 60,
-      "comment": "компенсация за возврат по причине брака",
-      "notify": True
+  def add_new_client(self, phone, email, fullName, gender, birthdate, card):
+    url = self.url + 'new-client'
+    data = {
+      "client": {
+        "phoneNumber": phone,
+        "email": email,
+        "fullName": fullName,
+        "gender": gender,
+        "birthdate": birthdate,
+        "card": card
+      }
     }
-  }
-  data_json = json.dumps(data)
-  r = requests.post(url=url,headers=headers, data=data_json)
-  respDTO = to_dto(r)
-  return respDTO
+    r = requests.post(url=url, headers=self.headers, json=data)
+    return self.to_dto(r)
 
-print(add_new_client(url, '+79774583899', 'mike@mail.ru', 'Mike CH', 1,
-                     '1999-11-21T00:00:00+04:00', '22222').description)
+  def send_code(self):
+    url = self.url + 'send-confirmation-code'
+    data = {
+      "phoneNumber": self.phone
+    }
+    r = requests.post(url=url,headers=self.headers, json=data)
+    respDTO = self.to_dto(r)
+    return respDTO
 
-print(get_balance(url,'+375296300643'))
-print(get_balance(url,'+79534706854'))
-# print(send_code(url, '+79774583897'))
-# print(change_balance(url, '+79774583897', 400))
+  def change_balance(self, delta):   
+    url = self.url + 'adjust-balance'
+    data = {
+      "client": {
+        "phoneNumber": self.phone
+      },
+      "balanceAdjustment": {
+        "amountDelta": delta,
+        "expirationPeriodDays": 60,
+        "comment": "компенсация за возврат по причине брака",
+        "notify": True
+      }
+    }
+    r = requests.post(url=url,headers=self.headers, json=data)
+    respDTO = self.to_dto(r)
+    return respDTO
+
+personOne = Maxma(url, secret_key, headers, phone)
+
+print(personOne.get_balance())
+# print(class1.add_new_client('+79774583895' ,'chetv@mail.ru', 'Mik CHetv', 1,
+#                      '1998-11-21T00:00:00+04:00', '2222235'))
+print(personOne.send_code())
+# print(class1.change_balance(50))
